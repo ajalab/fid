4134dea5b033291fbc3936b8503e3d9965cf2eda
Koki Kato <koki.kato1994@gmail.com>
2018-07-13 15:12:48 +0900

    Fix

--- src/fid.rs
+++ src/fid.rs
@@ -1,3 +1,4 @@
+/// A type that supports rank and support operations.
 pub trait FID {
     /// Returns the total number of bits.
     fn len(&self) -> u64;
@@ -13,22 +14,22 @@ pub trait FID {
 
     /// Compute the number of 0s in [0..i).
     fn rank0(&self, i: u64) -> u64 {
-        i + 1 - self.rank1(i)
+        i - self.rank1(i)
     }
 
     /// Compute the number of 0s in [0..i).
     fn rank1(&self, i: u64) -> u64 {
-        i + 1 - self.rank0(i)
+        i - self.rank0(i)
     }
 
-    /// Locate the position of the `(r + 1)`-th bit.
+    /// Locate the position of the (r + 1)-th bit.
     fn select(&self, b: bool, r: u64) -> u64 {
         let (mut s, mut e) = (0, self.len());
 
         while e - s > 1 {
             let m = (s + e) / 2;
-            let r = self.rank(b, m);
-            if r + 1 <= r {
+            let rank = self.rank(b, m);
+            if r + 1 <= rank {
                 e = m
             } else {
                 s = m
